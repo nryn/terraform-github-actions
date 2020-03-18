b888c5ba386245e7f9171ff1940435bb149e37e7
Narayan <narayan@ustwo.com>
2020-03-18 16:03:01 +0000

    Fix early exit and variable refs

--- src/terraform_graph.sh
+++ src/terraform_graph.sh
@@ -6,31 +6,31 @@ function terraformGraph {
   graphOutput=$(terraform graph ${*} 2>&1)
   graphExitCode=${?}
 
-  # Exit code of 0 indicates success. Print the output and exit.
+  # Exit code of 0 indicates success.
   if [ ${graphExitCode} -eq 0 ]; then
-    echo "graph: info: graph created"
-    echo "${graphOutput}"
-    echo
-    exit ${graphExitCode}
+    graphExitSummary = "info: graph created"
   fi
 
   # Exit code of !0 indicates failure.
   if [ ${graphExitCode} -ne 0 ]; then
-    echo "fmt: error: failed to create graph"
-    echo "${graphOutput}"
-    echo
-    exit ${graphExitCode}
+    graphExitSummary = "error: failed to create graph"
   fi
+
+  # Print the summary message and the graph output
+  echo "graph: ${graphExitSummary}"
+  echo "${graphOutput}"
 
   # Comment on the pull request if necessary.
   if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ]; then
-
-    graphCommentWrapper="#### \`terraform graph\` Failed
-*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*"
+    graphCommentWrapper="#### \`terraform graph\` ${graphExitSummary}
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*
+```
+${graphOutput}
+```"
 
     graphCommentWrapper=$(stripColors "${graphCommentWrapper}")
     echo "graph: info: creating JSON"
-    graphPayload=$(echo "${fmtCommentWrapper}" | jq -R --slurp '{body: .}')
+    graphPayload=$(echo "${graphCommentWrapper}" | jq -R --slurp '{body: .}')
     graphCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
     echo "graph: info: commenting on the pull request"
     echo "${graphPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${graphCommentsURL}" > /dev/null
